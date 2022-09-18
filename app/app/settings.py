ff429ced4484d496fe89e10883e154d6340b66a2
matheusses <matheus.s.es@gmail.com>
2022-09-17 21:42:06 -0300

    chore: fix ci

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,7 +81,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': 5432,
     }
 }
 
@@ -120,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
